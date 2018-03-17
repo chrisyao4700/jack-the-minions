5b72e260e3a22cafbe4a72ecb8d8c81176934ba0
Yuan Yao <yuanyao@Yuans-MacBook-Pro-7.local>
2018-03-17 15:56:31 -0700

    SAVE INIT COMPONENTS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,31 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './home/header/header.component';
+import { WorkComponent } from './home/work/work.component';
+import { AboutComponent } from './home/about/about.component';
+import { ContactComponent } from './home/contact/contact.component';
+import { WebsiteComponent } from './home/work/website/website.component';
+import { GraphicComponent } from './home/work/graphic/graphic.component';
+import { ArtComponent } from './home/work/art/art.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { TabComponent } from './home/work/tab/tab.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    WorkComponent,
+    AboutComponent,
+    ContactComponent,
+    WebsiteComponent,
+    GraphicComponent,
+    ArtComponent,
+    NavComponent,
+    TabComponent
   ],
   imports: [
     BrowserModule
